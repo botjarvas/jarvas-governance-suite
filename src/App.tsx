8fe757a43aaedcf798b4ad5ac4c124c1d9e48d7c
Jarvas <botjarvas@gmail.com>
2026-02-05 04:35:10 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"; // Mudamos para HashRouter
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -13,14 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* O basename é a peça que faltava para o GitHub Pages */}
-      <BrowserRouter basename="/jarvas-governance-suite">
+      <HashRouter> {/* HashRouter não precisa de basename para funcionar no GH Pages */}
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
